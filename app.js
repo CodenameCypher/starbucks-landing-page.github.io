5de1bdd38fa40e1fc084eb797dea56418e061f23
Aditya Roy <54760764+Einstein2193@users.noreply.github.com>
2021-01-19 19:41:26 +0600

    fixed circle and added hamburger menu

--- app.js
+++ app.js
@@ -19,3 +19,28 @@ button3.addEventListener("click",function(){
     document.querySelector(".images").style.transform = "rotate(-15deg)";
     document.querySelector(".circle").style.backgroundColor = '#d752b1';
 });
+
+let toggler = false;
+const hamburger = document.querySelector(".hamburger");
+const nav = document.querySelector(".navbar-items");
+const line1 = document.getElementById("1");
+const line2 = document.getElementById("2");
+const line3 = document.getElementById("3");
+hamburger.addEventListener("click",function() {
+    if(!toggler){
+        nav.style.display = 'flex';
+        line1.style.transform = 'rotate(45deg)';
+        line2.style.display = 'none';
+        line3.style.transform = 'rotate(-45deg)';
+        line3.style.position = 'fixed';
+        line3.style.top = '58px';
+        toggler = true;
+    }else{
+        nav.style.display = 'none';
+        line1.style.transform = 'rotate(0)';
+        line2.style.display = 'block';
+        line3.style.transform = 'rotate(0)';
+        line3.style.top = '78px';
+        toggler = false;
+    }
+});
